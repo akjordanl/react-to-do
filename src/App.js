0a88256ec89339bebab7cf5e2fc9bc3db546aa81
Andrew Jordan <akjordanl@gmail.com>
2018-11-16 19:21:44 -0500

    finished checkpoint react todo state and props

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import ToDo from './components/ToDo.js';
 // Import the ToDo component we created in a separate folder
 
 class App extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Wash the dishes', isCompleted: false },
+        { description: 'Wash baby bottles', isCompleted: false }
+      ]
+    };
+  }
+
   render() {
     return (
 
       <div className="App">
 
         <ul>
-          <ToDo />
-          <ToDo />
-          {/* Call to do component with funky tags and no commas */}
+
+          { this.state.todos.map(
+              function (todo, index) {
+                return  <ToDo key={ index } description={ todo.description } isCompleted = { todo.isCompleted }/>
+              }
+          )}
+          {/* Bring in todos from the state of the component */}
 
         </ul>
 
